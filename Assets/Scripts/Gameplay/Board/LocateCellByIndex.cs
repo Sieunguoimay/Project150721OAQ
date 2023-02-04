859a80c44fce3e0b5ad391bd38ab62cd3c1c3b70
Sieunguoimay <vuduydu1997@gmail.com>
2023-02-04 15:21:17 +0700

    b

--- Assets/Scripts/Gameplay/Board/LocateCellByIndex.cs
+++ Assets/Scripts/Gameplay/Board/LocateCellByIndex.cs
@@ -1,15 +1,32 @@
-﻿using UnityEngine;
+﻿using SNM;
+using UnityEngine;
 
 namespace Gameplay.Board
 {
     public class LocateCellByIndex : MonoBehaviour
     {
-        private int RingSize(int r) => 4 * r;
-        private int DiscSize(int r) => 1 + 2 * (r * r + r);
+        private int NumCellsOnRing(int r) => 4 * r;
+        private int NumCellsInDisc(int r) => 1 + 2 * (r * r + r);
 
-        private int LowerBoundRadiusFromDiscSize(int s)
+        private int LowerBoundRadiusFromNumCells(int s) => Mathf.FloorToInt(RadiusFromNumCells(s));
+        private int UpperBoundRadiusFromNumCells(int s) => Mathf.CeilToInt(RadiusFromNumCells(s));
+
+        private static float RadiusFromNumCells(int s)
         {
-             return (s - 2) / 2;
+            return (Mathf.Sqrt(2 * s - 1) - 1) / 2;
+        }
+
+        // public Vector2Int GetCellAtIndex(int index)
+        // {
+        //      
+        // }
+        [ContextMenu("Test")]
+          private void Test()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                Debug.Log(RadiusFromNumCells(i + 1));
+            }
         }
     }
 }
